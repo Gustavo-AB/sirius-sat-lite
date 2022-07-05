4bdd07355c9acc1b8ff5c222b24e239411a09703
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-07-05 18:27:16 -0300

    feat: creating mobile menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,7 @@
 // alert('hello world')
+const mobileButton = document.querySelector('button.mobile-button');
+const navbar = document.querySelector('header.container nav ul');
+
+mobileButton.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
